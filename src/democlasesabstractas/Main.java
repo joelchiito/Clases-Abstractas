20ebcf3cc82fe0e0c69145c9f7ab91b820541db0
Joel Enrique Mena May <16470287@itcampeche.edu.mx>
2017-06-02 00:13:08 -0500

    El proyecto se ha terminado con laimplementacion de la clase Main.

--- src/democlasesabstractas/Main.java
+++ src/democlasesabstractas/Main.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package democlasesabstractas;
-
+import modelo.Empleado;
+import modelo.EmpleadoAsalariado;
+import modelo.EmpleadoporHoras;
+import modelo.EmpleadoPorComision;
+import modelo.EmpleadoBaseMasComision;
 /**
  *
  * @author hp245
@@ -15,7 +19,40 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EmpleadoAsalariado empleadoAsalariado = new EmpleadoAsalariado ("John", "Smith", "111-11-1111", 800.0);
+        EmpleadoporHoras empleadoporHoras = new EmpleadoporHoras ("Karen", "Price", "222-22-2222", 16.75, 40);
+        EmpleadoPorComision empleadoPorComision = new EmpleadoPorComision ("Sue", "Jones", "333-33-3333", 10000, 0.06);
+        EmpleadoBaseMasComision empleadoBaseMasComision =new EmpleadoBaseMasComision ("Bob", "Lewis", "444-44-4444", 5000, .04, 300);
+        
+        System.out.println("Empleados procesados por separado:\n");
+        
+        System.out.printf("%s\n%s: $%,.2f\n\n", empleadoAsalariado, "ingresos", empleadoAsalariado.ingresos());
+        System.out.printf("%s\n%s: $%,.2f\n\n", empleadoporHoras, "ingresos", empleadoporHoras.ingresos());
+        System.out.printf("%s\n%s: $%,.2f\n\n", empleadoPorComision, "ingresos", empleadoPorComision.ingresos());
+        System.out.printf("%s\n%s: $%,.2f\n\n", empleadoBaseMasComision, "ingresos", empleadoBaseMasComision.ingresos());
+        
+        Empleado[] empleados = new Empleado [4];
+        
+        empleados[0] = empleadoAsalariado;
+        empleados[1] = empleadoporHoras;
+        empleados[2] = empleadoPorComision;
+        empleados[3] = empleadoBaseMasComision;
+        
+        System.out.println("Empleados procesados en forma polimorfica:\n");
+        
+        for (Empleado empleadoActual : empleados){
+            System.out.println(empleadoActual);
+            
+            if (empleadoActual instanceof EmpleadoBaseMasComision){
+            EmpleadoBaseMasComision empleado = (EmpleadoBaseMasComision) empleadoActual;
+            empleado.setSalarioBase( 1.10 * empleado.getSalarioBase());
+            System.out.printf("El nuevo salario base con 10%% de aumento es: $%,.2f\n", empleado.getSalarioBase());
+        }
+            
+            System.out.printf("ingresos $%,.2f\n\n", empleadoActual.ingresos());
+        }
+        
+        for (int j = 0; j < empleados.length; j++)
+            System.out.printf("El empleado %d es un %s\n", j, empleados[j].getClass().getName());
     }
-    
 }
--- src/modelo/EmpleadoBaseMasComision.java
+++ src/modelo/EmpleadoBaseMasComision.java
@@ -9,6 +9,33 @@ package modelo;
  *
  * @author hp245
  */
-public class EmpleadoBaseMasComision {
+public class EmpleadoBaseMasComision extends EmpleadoPorComision{
+    private double salarioBase;
     
+    public EmpleadoBaseMasComision( String nombre, String apellido, String nss, double ventas, double tarifa, double salario){
+        super(nombre, apellido, nss, ventas, tarifa);
+        setSalarioBase(salario);
+    }
+    
+    public void setSalarioBase(double salario){
+        if (salario >= 0.0)
+            salarioBase = salario;
+        else
+            throw new IllegalArgumentException("El salario base deber ser >= 0.0");
+    }
+    
+    public double getSalarioBase(){
+        return salarioBase;
+    }
+    
+    @Override
+    public double ingresos(){
+        return getSalarioBase() + super.ingresos();
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s %s; %s: $%,.2f", "con salario base de", super.toString(),
+                "salario base", getSalarioBase());
+    }
 }
